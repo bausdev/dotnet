81b61f1a17ee0c1f56700b314c9b884b9a76c4e4
Benjamin Fry <19857726+bausdev@users.noreply.github.com>
2021-01-31 06:04:11 -0600

    Updating main.js
    Adding some of the elements at the top

--- 3D_interface/start/main.js
+++ 3D_interface/start/main.js
@@ -1,3 +1,16 @@
+let scene, camera, rederer, sphere, material, bg,fb,composer, rgnPass, rmnt, filmParams, filmPass, renderPass, copyPass;
+
+let mouseX = 0, mouseY = 0, displaceSc = 50, shaderTime = 0, windowHalfX = window.innerWidth / 2, windowHalfY = window.innerHeight / 2;
+
+let init = () => {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcccccc);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+    
+    renderer = new THREE.WebGRenderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
 let params = () => {
     rmnt = 0.0024;
     rgbPass.uniforms['angle'].value = 1;
